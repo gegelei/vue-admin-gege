7ebf4883680c980c364fd6a36d6b3d03fb9afe73
陈海洋 <1559843332@qq.com>
2020-04-03 14:22:37 +0800

    递归

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="sidebar-container">
-        888444
         <el-scrollbar wrap-class="scrollbar-wrapper">
             <el-menu
                     router
@@ -52,7 +51,10 @@
                                     {
                                         "path": "/func331",
                                         "title": "功能3-3-1",
-                                        "children": []
+                                        "children": [{
+                                          "path": "/func3311",
+                                          "title": "功能3-3-1-1",
+                                        }]
                                     }
                                 ]
                             },
